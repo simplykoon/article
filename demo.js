53fbf214962085db80e01b2637764eb311a262ab
simplykoon <65523005+simplykoon@users.noreply.github.com>
2020-07-02 19:05:59 +0100

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,12 @@
+
+function showShare() {
+				
+ var x = document.getElementsByClassName("links");
+  if (x[0].style.display == "block") {
+    x[0].style.display = "none";
+  } 
+else {
+    x[0].style.display = "block";
+  }
+}
+
